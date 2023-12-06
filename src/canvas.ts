80f4dee9ca8b74ce3ca5539668b469bc3a8e9ad1
yanhaijing <yanhaijing@yeah.net>
2023-12-06 22:59:48 +0800

    feat: 添加基础能力

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,30 +1,51 @@
 import { EventEmitter } from '@jsmini/event';
 import { Shape } from './shape';
 
+export interface CanvasOption {
+  backgroundColor?: string;
+  devicePixelRatio?: number;
+}
 export class Canvas extends EventEmitter {
   private ctx: CanvasRenderingContext2D;
   private shapes: Shape[] = [];
   private width: number;
   private height: number;
+  private option!: CanvasOption;
 
   constructor(
     public canvas: HTMLCanvasElement,
-    public options?: {
-      backgroundColor?: string;
-    },
+    option?: CanvasOption,
   ) {
     super();
     this.ctx = canvas.getContext('2d')!;
     this.width = canvas.width;
     this.height = canvas.height;
+    this.set(option);
+  }
+  private setdpr() {
+    const { devicePixelRatio } = this.option;
+    if (devicePixelRatio && devicePixelRatio > 1) {
+      const { width, height } = this.canvas.getBoundingClientRect();
+      this.canvas.width = width * devicePixelRatio;
+      this.canvas.height = height * devicePixelRatio;
+      this.canvas.style.width = `${width}px`;
+      this.canvas.style.height = `${height}px`;
+      this.ctx.scale(devicePixelRatio, devicePixelRatio);
+    }
+  }
+  public set(option?: CanvasOption) {
+    this.option = { ...this.option, ...option };
+    this.setdpr();
   }
   public setWidth(width: number) {
     this.width = width;
     this.canvas.width = width;
+    this.setdpr();
   }
   public setHeight(height: number) {
     this.height = height;
     this.canvas.height = height;
+    this.setdpr();
   }
   public add(shape: Shape) {
     this.shapes.push(shape);
@@ -40,6 +61,12 @@ export class Canvas extends EventEmitter {
   }
   public render() {
     this.clear();
+
+    if (this.option?.backgroundColor) {
+      this.ctx.fillStyle = this.option.backgroundColor;
+      this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
     this.shapes.forEach((shape) => {
       if (shape instanceof Shape) {
         shape.render(this.ctx);
--- src/rect.ts
+++ src/rect.ts
@@ -1,6 +1,7 @@
 import { Shape } from './shape';
 
 export interface RectOption {
+  id?: string;
   x: number;
   y: number;
   width: number;
@@ -13,13 +14,34 @@ export interface RectOption {
   lineWidth?: number;
 }
 export class Rect extends Shape {
-  constructor(public option: RectOption) {
+  private option!: RectOption;
+  constructor(option: RectOption) {
     super();
+    this.set(option);
   }
   public set(option: Partial<RectOption>) {
     this.option = { ...this.option, ...option };
   }
   public render(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle;
+    ctx.save();
+    const option = this.option;
+
+    if (option.angle) {
+      const originX = option.originX || option.x + option.width / 2;
+      const originY = option.originY || option.y + option.height / 2;
+      ctx.translate(originX, originY);
+      ctx.rotate(option.angle);
+      ctx.translate(-originX, -originY);
+    }
+    if (option.fillStyle) {
+      ctx.fillStyle = option.fillStyle;
+      ctx.fillRect(option.x, option.y, option.width, option.height);
+    }
+    if (option.strokeStyle) {
+      ctx.strokeStyle = option.strokeStyle;
+      ctx.lineWidth = option.lineWidth || 1;
+      ctx.strokeRect(option.x, option.y, option.width, option.height);
+    }
+    ctx.restore();
   }
 }
